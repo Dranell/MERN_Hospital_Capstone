3263dd08a371b7f489f98a853b30700a75442a4f
Dranell <dranellwilson1@gmail.com>
2024-08-14 09:42:23 -0500

    specified unique idenifiers for url

--- server/index.js
+++ server/index.js
@@ -18,6 +18,9 @@ app.use(cors());
 
 const PORT = 3002
 
+const router = require("./Routes/patient-routes"); 
+app.use("/patient", router)
+
 //establish link to mongoDB Atlas Database
 
 mongoose.connect("mongodb+srv://dranellwilson1:Dranell.1wilson@cluster0.x8ixxam.mongodb.net/MERN_Health").then(() => console.log("Connected To Database"))  
