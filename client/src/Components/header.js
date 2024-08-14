8389b40b0d1911396ee668a4d8d73b80c2ed6645
Dranell <dranellwilson1@gmail.com>
2024-08-14 11:47:37 -0500

    Created Navigation Tool Bar Component.

---
+++ client/src/Components/header.js
@@ -0,0 +1,35 @@
+import React, {useState} from "react";
+import { AppBar, Tab, Tabs, Toolbar, Typography } from "@mui/material";
+import HomeIcon from '@mui/icons-material/Home';
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+    const [value, setValue] = useState();
+    return(
+        <div>
+            <AppBar sx = {{backgroundColor: "#232F3D"}} postion= "sticky">
+                <Toolbar>
+                    <NavLink to ="/" style={{color:white}}>
+                        <Typography>
+                            <HomeIcon/>
+                            <Tabs
+                            sx={{ml: "auto"}}
+                            textColor="inherit"
+                            indicatorColor="primary"
+                            value = {value}
+                            onChange={(e, val)=> setValue(val)}
+                            >
+                                <Tab LinkComponent={NavLink} to = "/add" label = "Add Patient"/>
+                                <Tab LinkComponent={NavLink} to = "/patient" label = "Patient Information"/>
+                            </Tabs>
+
+                            
+                        </Typography>
+                    </NavLink>
+                </Toolbar> 
+
+            </AppBar>
+        </div>
+)};
+
+export default Header;
